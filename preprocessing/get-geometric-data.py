b3f9ee405f478b10ec94d2121ea2ce8c084dbfcc
zmiller22 <zm93950@gmail.com>
2020-07-20 21:11:17 -0400

    saved

--- preprocessing/get-geometric-data.py
+++ preprocessing/get-geometric-data.py
@@ -27,7 +27,44 @@ def getGeometricFeatureDF(input_dir, output_path):
     swc_files_list = [f for f in listdir(input_dir) if isfile(join(input_dir, f))]
     swc_files_list = [input_dir+"/"+f for f in swc_files_list]
     
-    LMfeatures = pm.getMeasure(['Surface', 'Diameter'], swc_files_list)
+    #TODO split the feature names into the different groups for what measure
+    # should be extracted (average or total_sum) if necessary
     
-    return LMfeatures
+    total_feature_names = ["Soma_Surface", "N_stems", "N_bifs", "N_branch", 
+                           "N_tips", "Length", "TerminalSegment"]
+    avg_feature_names = ["Diameter", "EucDistance", "PathDistance", "Branch_Order",
+                         "Terminal_degree"]
+    feature_names = total_feature_names+avg_feature_names
     
+    LMfeatures = pm.getMeasure(feature_names, swc_files_list)
+    
+    #TODO export geometric featurez into a dataframe
+    df = pd.DataFrame()
+    for i, feature in enumerate(LMfeatures):
+        
+        feature_dict = feature["WholeCellMeasuresDict"]
+        
+        if feature_names[i] in avg_feature_names:
+            nrn_feature_list = [nrn["Average"] for nrn in feature_dict]
+            df[ feature_names[i] ] = nrn_feature_list
+        elif feature_names[i] in total_feature_names:
+            nrn_feature_list = [nrn["TotalSum"] for nrn in feature_dict]
+            df[ feature_names[i] ] = nrn_feature_list
+            
+    # Set the dataframe names to be the neuron names
+    row_names = [os.path.basename(path).strip(".swc") for path in swc_files_list]
+    df.index = row_names
+    
+    
+    return df
+    
+#%%
+test = getGeometricFeatureDF("/home/zack/Desktop/Lab_Work/Projects/neuron-morphology-classifier/testing/test_nrns", "test")
+
+#%%
+df = pd.DataFrame()
+for i, feature in enumerate(LMfeatures):
+    feature_dict = feature["WholeCellMeasuresDict"]
+    nrn_feature_list = [nrn["Average"] for nrn in feature_dict]
+    
+    df[ feature_names[i] ] = nrn_feature_list
